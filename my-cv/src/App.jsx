3f3d92b504b511a46d7ff72e6130132cba7ea119
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-04 00:41:17 +0100

    FEAT new buttons useState in App to show educacito and experience

--- my-cv/src/App.jsx
+++ my-cv/src/App.jsx
@@ -1,6 +1,6 @@
+import { useState } from "react";
 import './App.css';
 
-/* import { Heroine, About, Education, Experience, More } from './components'; */
 import Heroine from './components/Heroine';
 import About from './components/About';
 import Education from './components/Education';
@@ -12,13 +12,26 @@ import { CV } from './CV/CV';
 const { heroine, education, books, exhibitions, lecturer, design, languages, habilities, awards, volunteer } = CV;
 
 function App() {
+  const [showEducation, setShowEducation] = useState(true);
+ 
   return (
     <div className='App'>
     <main>
        <Heroine heroine={heroine} />
        <div className="infoMain">
        <About aboutMe={heroine.aboutMe} />
-       <Education education={education} />
+       
+    <button onClick={()=> setShowEducation(true)}>Education</button>
+    <button onClick={()=>setShowEducation(false)}>Experience</button>
+
+    <div>
+      {showEducation ? (
+        <Education education={education}/>
+      ) : (
+        <Experience experience={experience} />
+      )}
+    </div>
+
        <Experience 
          books={books} 
          exhibitions={exhibitions}
