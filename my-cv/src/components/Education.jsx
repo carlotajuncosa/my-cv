0c27fab0b55fe1468dc2a38f57fbdf5ce5575fba
carlotajuncosa <carlota.juncosa@gmail.com>
2022-11-09 20:06:33 +0100

    FEAT. Render education info. FIX classNAme instead of class in About.jsx icons and color in Navbar.scss 900 width responsive

--- my-cv/src/components/Education.jsx
+++ my-cv/src/components/Education.jsx
@@ -1,20 +1,32 @@
 import React, {useContext} from "react";
 import { MyContext } from "../Shared/Context";
+import "./Education.scss"
 
-const Education = ({ education }) => {
+const Education = () => {
+  const { education } = useContext(MyContext);
+  //console.log('THIS IS EDUCATION IN EDUCATION JSX:', education);
   return (
     <div>
       <h3>Education</h3>
-      {/* {education.map((edu) => {
-        return (
+     {education.map((edu)=>{
+        return(
           <div key={JSON.stringify(edu)}>
             <h4>{edu.name}</h4>
             <p>{edu.date} | {edu.where}</p>
           </div>
-        );
-      })} */}
+        )
+      })} 
     </div>
   );
 };
 
 export default Education;
+
+/* {{education.map((edu) => {
+  return (
+    <div key={JSON.stringify(edu)}>
+      <h4>{edu.name}</h4>
+      <p>{edu.date} | {edu.where}</p>
+    </div>
+  );
+})} */
